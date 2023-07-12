785cdeb6417a691f06d955e89ed5f9b1a75f3dbb
Gaetan Lepage <gaetan@glepage.com>
2023-07-12 09:49:42 +0200

    changes

--- flake.nix
+++ flake.nix
@@ -1,9 +1,14 @@
 {
-  # inputs.nixpkgs.url = "/home/gaetan/perso/nix/nixpkgs";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  inputs.nixvim = {
-    url = "/home/gaetan/perso/nix/nixvim/nixvim";
-    inputs.nixpkgs.follows = "nixpkgs";
+  inputs = {
+    # nixpkgs.url = "/home/gaetan/perso/nix/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixvim = {
+      # url = "/home/gaetan/perso/nix/nixvim/nixvim";
+      url = "github:pta2002/nixvim";
+      # url = "github:pta2002/nixvim/nixos-23.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
@@ -12,7 +17,11 @@
     nixvim,
   }: let
     system = "x86_64-linux";
-    # pkgs = import nixpkgs {inherit system;};
+    pkgs = import nixpkgs {inherit system;};
+    # pkgs-unstable = import nixpkgs {
+    #   inherit system;
+    #   config.allowUnfree = true;
+    # };
 
     nixvim' = nixvim.legacyPackages.${system};
     nvim =
@@ -20,17 +29,14 @@
       {
         # inherit pkgs;
         module = {
-          filetype.extension."typ" = "typst";
           plugins = {
-            # coq-nvim.enable = true;
-            lsp = {
-              enable = true;
-
-              servers = {
-                typst-lsp.enable = true;
-                bashls.enable = true;
-              };
-            };
+            lualine.enable = true;
+            # lsp = {
+            #   enable = true;
+            #   servers = {
+            #     nil_ls.enable = true;
+            #   };
+            # };
           };
         };
       };
