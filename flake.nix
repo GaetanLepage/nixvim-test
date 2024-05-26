636fb1e24e1e21ee746b6db8e801f3f346f7de35
Gaetan Lepage <gaetan@glepage.com>
2024-05-26 14:23:15 +0200

    lsp-format

--- flake.nix
+++ flake.nix
@@ -1,13 +1,19 @@
 {
   inputs = {
-    nixpkgs.url = "/home/gaetan/perso/nix/nixpkgs";
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "/home/gaetan/perso/nix/nixpkgs";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github:nixos/nixpkgs";
     # nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     nixvim = {
-      url = "/home/gaetan/perso/nix/nixvim/nixvim";
-      # url = "github:nix-community/nixvim";
+      # url = "/home/gaetan/perso/nix/nixvim/nixvim";
+      url = "github:nix-community/nixvim";
       # url = "github:nix-community/nixvim/nixos-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
     };
   };
 
@@ -15,32 +21,52 @@
     self,
     nixpkgs,
     nixvim,
-  }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {inherit system;};
+    flake-parts,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = nixpkgs.lib.systems.flakeExposed;
 
-    nixvim' = nixvim.legacyPackages.${system};
-    nvim =
-      nixvim'.makeNixvimWithModule
-      {
-        # inherit pkgs;
-        module = {helpers, ...}: {
-          colorschemes.gruvbox.enable = true;
-          plugins = {
-            indent-blankline.enable = true;
+      perSystem = {
+        pkgs,
+        inputs',
+        ...
+      }: {
+        formatter = pkgs.alejandra;
+        packages = let
+          # lsp-format-nvim = pkgs.vimUtils.buildVimPlugin {
+          #   name = "lsp-format-nvim";
+          #   src = /home/gaetan/temp/lsp-format.nvim;
+          #   # src = pkgs.fetchFromGitHub {
+          #   #   owner = "lukas-reineke";
+          #   #   repo = "lsp-format.nvim";
+          #   #   rev = "3612642b0e2eb85015838df5dcfbacb61f15db98";
+          #   #   sha256 = "1pizkn16ma7yfsy19g06f6l6zkqwsjkmzybqhhhp18xbbyj7m8cc";
+          #   # };
+          # };
+        in {
+          # inherit lsp-format-nvim;
+          default = inputs'.nixvim.legacyPackages.makeNixvimWithModule {
+            inherit pkgs;
+            module = {pkgs, ...}: {
+              plugins = {
+                lsp-format = {
+                  enable = true;
+                  # package = lsp-format-nvim;
+                  lspServersToEnable = ["tinymist"];
+                  setup.typst.force_attach = true;
+                };
+                lsp = {
+                  enable = true;
+                  servers.tinymist = {
+                    enable = true;
+                    settings.formatterMode = "typstyle";
+                  };
+                };
+              };
+            };
           };
-          keymaps = [
-            {
-              mode = "n";
-              key = "<C-a>";
-              action = "function() print('hello') end";
-              lua = true;
-            }
-          ];
         };
       };
-  in {
-    packages.${system}.default = nvim;
-    formatter.${system} = nixpkgs.legacyPackages.${system}.alejandra;
-  };
+    };
 }
